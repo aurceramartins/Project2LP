142a784e8ac7c688d573822b19b5d4a4ee073c54
Joana Marques <joana.pipaa@gmail.com>
2018-06-19 17:04:44 +0100

    Add Tiles class

---
+++ Projecto2LP/Tiles.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projecto2LP
+{
+    class Tiles : List<Object>
+    {
+        public bool TileExploration { get; set; }
+
+        public Tiles() : base(10)
+        {
+            ResetTiles();
+        }
+
+        public IEnumerator<Object> GetTiles()
+        {
+            foreach (Object obj in this)
+            {
+                yield return obj;
+            }
+        }
+
+        public void ResetTiles()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Add(null);
+                TileExploration = false;
+            }
+        }
+    }
+}
