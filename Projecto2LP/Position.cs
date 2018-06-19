d74cf7d0743ce5477acdd1a5f467f27225276378
Pedro Santos <pedrosantosobral@gmail.com>
2018-06-19 16:06:05 +0100

    Adicionar classe Position

---
+++ Projecto2LP/Position.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projecto2LP
+{
+    class Position
+    {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
