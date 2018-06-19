e79ae157bdc3d4d47126e75f5bf801986f2361cd
Pedro Santos <pedrosantosobral@gmail.com>
2018-06-19 23:34:37 +0100

    Adicionar classe Items

---
+++ Projecto2LP/Items.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projecto2LP
+{
+    public interface IItems
+    {
+        float Weight { get; set; }
+        void RandPos();
+    }
+}
