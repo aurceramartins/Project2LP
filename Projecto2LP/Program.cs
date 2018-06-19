fd7d04525e362cda5cea46aed62f2b060726cbef
Urcera <aurceramartins@danielcastelao.org>
2018-06-19 11:41:31 +0200

    first commit

---
+++ Projecto2LP/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Projecto2LP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameLoop gl = new GameLoop();
+            gl.Play();
+        }
+    }
+}
