84f4b6eab8e81cd836eb0bda269e964452b847c2
Pedro Santos <pedrosantosobral@gmail.com>
2018-06-19 16:41:51 +0100

    Adicionar Board

---
+++ Projecto2LP/Board.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projecto2LP
+{
+    class Board
+    {
+        public Tiles[,] BoardTiles { get; }
+
+        public Board()
+        {
+            BoardTiles = new Tiles[8, 8];
+            ResetBoard();
+        }
+        public void ResetBoard()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    BoardTiles[i, j] = new Tiles();
+                }
+            }
+        }
+    }
+}
