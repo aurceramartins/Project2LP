a7f5d59f1551894da7f68b3ef514a619e4175aba
Joana Marques <joana.pipaa@gmail.com>
2018-06-19 16:14:15 +0100

    Add Exit class

---
+++ Projecto2LP/Exit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projecto2LP
+{
+    class Exit
+    {
+
+        public bool FindExit { get; set; }
+        public Position ExitPos { get; set; }
+        public Exit()
+        {
+            FindExit = false;
+            ExitPos = ExitRandPos();
+
+        }
+
+        public Position ExitRandPos()
+        {
+            Random rnd = new Random();
+            int startPos = rnd.Next(0, 8);
+            ExitPos = new Position(startPos, 7);
+            return ExitPos;
+        }
+
+        public void CheckExit(Board b, int row, int col)
+        {
+            int count = 0;
+            foreach (Object ob in b.BoardTiles[row,col])
+            {
+                if(ob is Player) { count++; }
+                if(ob is Exit) { count++; }
+
+                if(count == 2) { FindExit = true; count = 0; }
+            }
+            count = 0;
+        }
+    }
+}
