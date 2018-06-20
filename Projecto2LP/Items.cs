4dec248e5725ad1bf018a52c3f82415269f4ccd7
Urcera <aurceramartins@danielcastelao.org>
2018-06-20 14:36:38 +0200

    commits

--- Projecto2LP/Items.cs
+++ Projecto2LP/Items.cs
@@ -4,9 +4,18 @@ using System.Text;
 
 namespace Projecto2LP
 {
+    /// <summary>
+    /// Interface IItems.
+    /// </summary>
     public interface IItems
     {
+        /// <summary>
+        /// Propertie float IItems.Weight.
+        /// </summary>
         float Weight { get; set; }
+        /// <summary>
+        /// Method IItems.RandPos
+        /// </summary>
         void RandPos();
     }
 }
