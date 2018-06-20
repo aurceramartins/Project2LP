04202b04544351dbcaf084eaa0275a637c9ab397
Urcera <aurceramartins@danielcastelao.org>
2018-06-20 14:37:19 +0200

    commits

--- Projecto2LP/Position.cs
+++ Projecto2LP/Position.cs
@@ -4,11 +4,24 @@ using System.Text;
 
 namespace Projecto2LP
 {
+    /// <summary>
+    /// Class Position. The positions that contais the board.
+    /// </summary>
     class Position
     {
+        /// <summary>
+        /// propertie Row
+        /// </summary>
         public int Row { get; }
+        /// <summary>
+        /// Porpertie Column
+        /// </summary>
         public int Column { get; }
-
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="column"></param>
         public Position(int row, int column)
         {
             Row = row;
