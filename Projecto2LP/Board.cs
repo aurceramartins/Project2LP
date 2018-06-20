e2687ffa8259331f79b0eb13f5a662fd3f149a82
Urcera <aurceramartins@danielcastelao.org>
2018-06-20 14:35:07 +0200

    commits

--- Projecto2LP/Board.cs
+++ Projecto2LP/Board.cs
@@ -4,15 +4,26 @@ using System.Text;
 
 namespace Projecto2LP
 {
+    /// <summary>
+    /// Class Board
+    /// </summary>
     class Board
     {
+        /// <summary>
+        /// Propertie Tiles[,] BoardTiles
+        /// </summary>
         public Tiles[,] BoardTiles { get; }
-
+        /// <summary>
+        /// Constructor Board
+        /// </summary>
         public Board()
         {
             BoardTiles = new Tiles[8, 8];
             ResetBoard();
         }
+        /// <summary>
+        /// Method ResetBoard. Set to null all the positions in the board.
+        /// </summary>
         public void ResetBoard()
         {
             for (int i = 0; i < 8; i++)
