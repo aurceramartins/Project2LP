7753ff543b75234330accccb4fff9cae949be506
Urcera <aurceramartins@danielcastelao.org>
2018-06-20 14:37:43 +0200

    commits

--- Projecto2LP/Tiles.cs
+++ Projecto2LP/Tiles.cs
@@ -4,15 +4,26 @@ using System.Text;
 
 namespace Projecto2LP
 {
+    /// <summary>
+    /// class Tiles
+    /// </summary>
     class Tiles : List<Object>
     {
+        /// <summary>
+        /// Propertie bool TileExploration
+        /// </summary>
         public bool TileExploration { get; set; }
-
+        /// <summary>
+        /// Constructor Tiles
+        /// </summary>
         public Tiles() : base(10)
         {
             ResetTiles();
         }
-
+        /// <summary>
+        /// Method GetTiles. Return al the tiles in each boar cell.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerator<Object> GetTiles()
         {
             foreach (Object obj in this)
@@ -20,7 +31,9 @@ namespace Projecto2LP
                 yield return obj;
             }
         }
-
+        /// <summary>
+        /// Method ResetTiles. Reset all the tiles to null.
+        /// </summary>
         public void ResetTiles()
         {
             for (int i = 0; i < 10; i++)
